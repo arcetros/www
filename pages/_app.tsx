333706d5f471ef0320dfa9ccd916d8b01d288acf
arcetros <75669794+arcetros@users.noreply.github.com>
2022-10-26 17:17:26 +0800

    fix: eslint errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
 import '../styles/globals.css'
-
 import '@fontsource/inter/300.css'
 import '@fontsource/inter/400.css'
 import '@fontsource/inter/500.css'
 import '@fontsource/inter/700.css'
-
 
 import type { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
